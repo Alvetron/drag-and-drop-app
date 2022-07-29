15ee14d61709972dcee3e8b6c9abd6f200f14b71
Andrii <thesparkylegendary@gmail.com>
2022-07-29 11:22:48 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,44 @@
+let draggables = document.querySelectorAll('.draggable');
+let containers = document.querySelectorAll('.container');
+
+draggables.forEach(draggable => {
+
+  draggable.addEventListener('dragstart', (e) => {
+    draggable.classList.add('dragging');
+  })
+
+  draggable.addEventListener('dragend', (e) => {
+    draggable.classList.remove('dragging');
+  })
+
+})
+
+containers.forEach(container => {
+  container.addEventListener('dragover', e => {
+    e.preventDefault();
+    const afterElement = getDragAfterElement(container, e.clientY);
+    console.log(afterElement)
+    const draggable = document.querySelector('.dragging');
+    if(afterElement == null) {
+      container.appendChild(draggable)
+    } else {
+      container.insertBefore(draggable, afterElement)
+    }
+  })
+})
+
+function getDragAfterElement(container, y) {
+  const draggableEl = [...container.querySelectorAll('.draggable:not(.dragging)')];
+
+  return draggableEl.reduce((closest, child) => {
+    const box = child.getBoundingClientRect();
+    const offset  = y - box.top - box.height / 2;
+    console.log(offset)
+    if(offset < 0 && offset > closest.offset) {
+      return {offset: offset, element: child};
+    }
+    else {
+      return closest;
+    }
+  }, { offset: Number.NEGATIVE_INFINITY }).element
+}
